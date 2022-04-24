7420b9cc98215a7d877e1c88746df6a1aec8d21c
Loren M. Lang <lorenl@north-winds.org>
2022-04-23 19:38:39 -0700

    Take in summits via an InputStream
    This will make it easier to move the code to using a raw resource in Android.

--- app/src/test/java/org/northwinds/app/sotachaser/SummitListTest.kt
+++ app/src/test/java/org/northwinds/app/sotachaser/SummitListTest.kt
@@ -2,12 +2,13 @@ package org.northwinds.app.sotachaser
 
 import com.opencsv.bean.CsvToBeanBuilder
 import com.opencsv.bean.HeaderColumnNameMappingStrategy
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 import java.nio.file.Paths
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
 
 class SummitListTest {
     @Test
@@ -15,27 +16,25 @@ class SummitListTest {
         val fileName = "src/main/res/raw/summitslist.csv"
         val myPath = Paths.get(fileName)
 
-        Files.newBufferedReader(myPath, StandardCharsets.UTF_8).use {
+        val input = Files.newInputStream(myPath)
+        BufferedReader(InputStreamReader(input, StandardCharsets.UTF_8)).use {
     	    it.readLine()
             val strategy = HeaderColumnNameMappingStrategy<Summit>()
-            strategy.setType(Summit::class.java)
+            strategy.type = Summit::class.java
 
             val csvToBean = CsvToBeanBuilder<Summit>(it)
                     .withMappingStrategy(strategy)
                     .withIgnoreLeadingWhiteSpace(true)
                     .build()
 
-            val cars = csvToBean.parse()
+            val summits = csvToBean.parse()
             //cars[0].setName("داۇد ")
-            cars.forEach(System.err::println)
+            summits.forEach(System.err::println)
 
-            //val names = cars.map { it.summitCode }.toSet()
-            //val regions = cars.map { it.summitCode.split("-")[0] }.toSet()
-            //val associations = cars.map { it.summitCode.split("/")[0] }.toSet()
-            val names = cars.associateBy({ it.summitCode }, { it.summitName })
-            val regions = cars.associateBy({ it.summitCode.split("-")[0] }, { it.regionName })
-            val associations = cars.associateBy({ it.summitCode.split("/")[0] }, { it.associationName })
-            assertEquals(164345, cars.count(), "Incorrect number of summits")
+            val names = summits.associateBy({ it.summitCode }, { it.summitName })
+            val regions = summits.associateBy({ it.summitCode.split("-")[0] }, { it.regionName })
+            val associations = summits.associateBy({ it.summitCode.split("/")[0] }, { it.associationName })
+            assertEquals(164345, summits.count(), "Incorrect number of summits")
             assertEquals(164345, names.count(), "Incorrect number of summit designators")
             assertEquals(1422, regions.count(), "Incorrect number of summit regions")
             assertEquals(194, associations.count(), "Incorrect number of summit associations")
@@ -43,9 +42,7 @@ class SummitListTest {
             assertEquals("Ófæruhöfði", names["TF/SL-089"])
             assertEquals("Ajman - عجمان", regions["A6/AJ"])
             assertEquals("Brazil - Maranhão", associations["PR8"])
-            //val summits_by_region = cars.groupBy({ it.summitCode.split("-")[0] })
-            //val regions_by_association = summits_by_region.groupBy({ it.summitCode.split("/")[0] })
-            val regions_by_association = cars.groupBy { it.summitCode.split("/")[0] }
+            val regions_by_association = summits.groupBy { it.summitCode.split("/")[0] }
             val summits_by_region = regions_by_association.mapValues { it.value.groupBy { it.summitCode.split("-")[0] } }
             assertEquals(194, summits_by_region.count(), "Incorrect number of associations")
             assertEquals(10, summits_by_region["W7O"]!!.count(), "Incorrect number of regions in Oregon association")
